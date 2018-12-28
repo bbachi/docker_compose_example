23c7b1ab43af22b0652c2ec3158124d78c7abe23
Bhargav Bachina <bhargavbachina@S162445.local>
2018-12-27 20:49:49 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server'
+});
+client.set("visits", 0);
+
+app.get("/", (req, res) => {
+    client.get("visits", (err,visits) => {
+        res.send("Number of visists is " + visits);
+        client.set("visits", parseInt(visits)+1);
+    })
+});
+
+app.listen("8081", () => {
+    console.log("app listening on port 8081....");
+});
